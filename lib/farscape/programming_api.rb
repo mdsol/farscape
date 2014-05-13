2f91f5dadebd54c04345ef5f4f1ca64b3f6f5092
Jordi Carres <jcarres@mdsol.com>
2014-05-13 11:00:27 +0900

    get discovery out till interface is done

--- lib/farscape/programming_api.rb
+++ lib/farscape/programming_api.rb
@@ -1,17 +1,12 @@
 
 module Farscape
 
-  #This is the external API for the users of farscape.
+  # This is the external API for the users of farscape.
+  # It is supposed to be more constraint but very straight-forward to use.
   class << self
     # Returns the configuration of the Farscape functionality
     def config
       @config ||= ConfigFile.new.configuration
-    end
-
-    # Discovers and returns the representation as an object of the entry point of a resource
-    # @param [String] resource_name : resource name to discover.
-    def discover(resource_name, options = {})
-      DiscoveryClient.new.discover(resource_name, options)
     end
 
     # Gets and resturns the respresentation as an object of some url which should have a document
