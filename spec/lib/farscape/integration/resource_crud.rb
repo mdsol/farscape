2ede2ed9b2e9804b068600f2c7965cdf380bbbff
Connor Savage <csavage@mdsol.com>
2014-12-01 18:20:02 -0500

    remove unecessary return

--- spec/lib/farscape/integration/resource_crud.rb
+++ spec/lib/farscape/integration/resource_crud.rb
@@ -2,19 +2,16 @@ require 'spec_helper'
 
 describe Farscape::Representor do
   let(:entry_point) { "http://localhost:#{RAILS_PORT}"}
-  
+
   let(:drds_link) { Farscape::Agent.new(entry_point).enter.transitions["drds"] }
-  
+
   describe 'can do anything' do
     it 'returns the entry point' do
-      drds_resource = drds_link.invoke do |args| 
-        args.parameters = {conditions: 'can_do_anything'}
-        args
-      end
+      drds_resource = drds_link.invoke { |args| args.parameters = {conditions: 'can_do_anything'} }
       expect(drds_resource.transitions.keys).to include('create')
     end
   end
-  
+
   # start at the entry entry_point
   # get the drds (list) resource
   # create a resource
@@ -23,5 +20,5 @@ describe Farscape::Representor do
   # it can toggle activation
   # it can update
   # it can delete
-  
+
 end
