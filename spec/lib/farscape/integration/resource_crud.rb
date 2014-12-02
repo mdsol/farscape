1f5a04e7e0b0a4c48180a05748de4fdfa5dd9b8f
Connor Savage <csavage@mdsol.com>
2014-12-01 19:06:45 -0500

    add create spec

--- spec/lib/farscape/integration/resource_crud.rb
+++ spec/lib/farscape/integration/resource_crud.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
+require 'json'
 
 describe Farscape::Representor do
   let(:entry_point) { "http://localhost:#{RAILS_PORT}"}
 
   let(:drds_link) { Farscape::Agent.new(entry_point).enter.transitions["drds"] }
+  let(:can_do_hash) { {conditions: 'can_do_anything'} }
 
   describe 'can do anything' do
-    it 'returns the entry point' do
-      drds_resource = drds_link.invoke { |args| args.parameters = {conditions: 'can_do_anything'} }
+    it 'includes appropriate transitions' do
+      drds_resource = drds_link.invoke { |req| req.parameters = can_do_hash }
       expect(drds_resource.transitions.keys).to include('create')
+    end
+
+    it 'can create a drd' do
+      name = 'Angry Max'
+      drds_resource = drds_link.invoke { |req| req.parameters = can_do_hash }
+      drd = drds_resource.transitions['create'].invoke { |req| req.parameters = {name: name} }
+      expect(drd.transitions['self'].invoke.attributes['name']).to eq(name)
     end
   end
 
