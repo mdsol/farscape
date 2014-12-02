b6022d875180af9d46ad86c19fbc1f4753986654
Connor Savage <csavage@mdsol.com>
2014-12-01 21:53:26 -0500

    add delete draft scenario

--- spec/lib/farscape/integration/resource_crud.rb
+++ spec/lib/farscape/integration/resource_crud.rb
@@ -7,7 +7,7 @@ describe Farscape::Representor do
   let(:drds_link) { Farscape::Agent.new(entry_point).enter.transitions["drds"] }
   let(:can_do_hash) { {conditions: 'can_do_anything'} }
 
-  describe 'can do anything' do
+  context 'can do anything' do
     it 'includes appropriate transitions' do
       drds_resource = drds_link.invoke { |req| req.parameters = can_do_hash }
       expect(drds_resource.transitions.keys).to include('create')
@@ -18,6 +18,23 @@ describe Farscape::Representor do
       drds_resource = drds_link.invoke { |req| req.parameters = can_do_hash }
       drd = drds_resource.transitions['create'].invoke { |req| req.parameters = {name: name} }
       expect(drd.transitions['self'].invoke.attributes['name']).to eq(name)
+    end
+
+    context 'an existing drd' do
+      before do
+        drds_resource = drds_link.invoke { |req| req.parameters = can_do_hash }
+        @drd = drds_resource.transitions['create'].invoke { |req| req.parameters = params }
+      end
+
+      let(:name) { 'Brave New DRD' }
+      let(:params) { {name: name}.merge(can_do_hash) }
+
+      it 'can delete a drd' do
+        #TODO fix this, expects self to include query string, otherwise identical
+        #expect(@drd.transitions['self'].invoke { |r| r.parameters = can_do_hash }.to_hash).to eq(@drd.to_hash)
+        @drd.transitions['destroy'].invoke
+        expect(@drd.transitions['self'].invoke { |r| r.parameters = can_do_hash }.to_hash).to_not eq(@drd.to_hash)
+      end
     end
   end
 
